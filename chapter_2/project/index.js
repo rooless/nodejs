5a0cc0bd0df942800907d6a49d07f93f43b11495
Helena Siniakova <i.rooless@gmail.com>
2018-12-01 02:15:25 +0300

    Создала простой модуль, опубликовала его, и установила, проверила его работу.

---
+++ chapter_2/project/index.js
@@ -0,0 +1,2 @@
+var rooless = require('roolessmodule');
+rooless();
---
+++ chapter_2/project/node_modules/roolessmodule/index.js
@@ -0,0 +1,4 @@
+module.exports = function() {
+    console.log("Этот модуль - самый первый мой модуль");
+    console.log("Помогает рисовать картины");
+};
